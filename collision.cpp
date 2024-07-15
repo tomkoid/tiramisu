dbf30fa3faf393daeba0cf5519e6161af4d1ec30
Tomkoid <tomaszierl@outlook.com>
2024-07-15 12:38:57 +0200

    collision: reset position if already outside of bounds
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- collision.cpp
+++ collision.cpp
@@ -2,18 +2,25 @@
 #include <iostream>
 #include <raylib.h>
 
+const int RESET_OFFSET = 1;
+
 void collision(Vector2 *ballPosition, float &accel_x, float &accel_y) {
+  // reverse direction
   if (ballPosition->y + CIRCLE_RAD >= screenHeight) {
     std::cout << "direction: reverse accel_y" << std::endl;
+    ballPosition->y = screenHeight - CIRCLE_RAD - RESET_OFFSET;
     accel_y = -accel_y;
   } else if (ballPosition->x + CIRCLE_RAD >= screenWidth) {
     std::cout << "direction: reverse accel_x" << std::endl;
+    ballPosition->x = screenWidth - CIRCLE_RAD - RESET_OFFSET;
     accel_x = -accel_x;
   } else if (ballPosition->y - CIRCLE_RAD <= 0) {
     std::cout << "direction: reverse accel_y" << std::endl;
+    ballPosition->y = CIRCLE_RAD + RESET_OFFSET + 0;
     accel_y = -accel_y;
   } else if (ballPosition->x - CIRCLE_RAD <= 0) {
     std::cout << "direction: reverse accel_x" << std::endl;
+    ballPosition->x = CIRCLE_RAD + RESET_OFFSET + 0;
     accel_x = -accel_x;
   }
 }
